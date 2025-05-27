ae0de0c9ddb11e3d6a35f78973e82629ae709377
cvdante2020 <dante_emc@hotmail.com>
2025-05-27 08:51:30 -0500

    Enfoque SAAS V3

--- src/app/autos/page.tsx
+++ src/app/autos/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import PlanesAutos from "@/components/PlanesAutos";
 import ContactoForm from "@/components/ContactoForm";
--- src/app/clinicas/page.tsx
+++ src/app/clinicas/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import PlanesClinicas from "@/components/PlanesClinicas";
 import ContactoForm from "@/components/ContactoForm";
